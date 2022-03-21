2193165ace86250a87948dafb3f858c69a429258
Михаил Устьянцев <xcreyan@gmail.com>
2022-03-21 17:32:44 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class CabinetStomatology: IItems
+        {
+            public CabinetStomatology(int _numberDoctors, int _numberPriems)
+            {
+                numberDoctors = _numberDoctors;
+                numberPriems = _numberPriems;
+            }
+            public int numberDoctors { get; set; }
+            public int numberPriems { get; set; }
+            public virtual void message()
+            {
+                MessageBox.Show($"Количество докторов: {numberDoctors}\nКоличество завершенных приёмов: {numberPriems}");
+            }
+        }
+        class Priems: CabinetStomatology, IItems
+        {
+            double x;
+            public Priems(int _numberDoctors, int _numberPriems) : base(_numberDoctors, _numberPriems)
+            {
+                if (numberDoctors != 0)
+                {
+                    x = Convert.ToDouble(numberPriems) / Convert.ToDouble(numberDoctors);
+                }
+                else
+                {
+                    MessageBox.Show("На ноль делить нельзя! Заполните количество докторов");
+                }
+            }
+            public override void message()
+            {
+                if (numberDoctors != 0)
+                {
+                    MessageBox.Show($"Примерно приёмов на одного доктора: {x}");
+                }
+                else 
+                { 
+                    MessageBox.Show("На ноль делить нельзя! Заполните количество докторов");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength > 0 && textBox2.TextLength > 0)
+            {
+                CabinetStomatology cabinetStomatology = new CabinetStomatology(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+                cabinetStomatology.message();
+            }
+            else 
+            { 
+                MessageBox.Show("Заполните все поля");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength > 0 && textBox2.TextLength > 0)
+            {
+                Priems priems = new Priems(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+                priems.message();
+            }
+            else
+            {
+                MessageBox.Show("Заполните все поля");
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp2/Interface1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    interface IItems
+    {
+        void message();
+        int numberDoctors { get; set; }
+        int numberPriems { get; set; }
+    }
+}
